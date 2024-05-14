f04448fb33aa09e533604161d146a9140fcd030e
Diego Saavedra <dmsaavedra88@gmail.com>
2024-05-13 19:44:42 -0500

    correction bugs

--- productos/urls.py
+++ productos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'productos'
+
 urlpatterns = [
     path('', views.listar_productos, name='listar_productos'),
     path('agregar/', views.agregar_producto, name='agregar_producto'),
-    path('actualizar/', views.actualizar_producto, name='actualizar_producto'),
+    path('actualizar/<int:id>/', views.actualizar_producto, name='actualizar_producto'),
     path('eliminar/', views.eliminar_producto, name='eliminar_producto'),
     path('buscar/', views.buscar_producto, name='buscar_producto'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from pyexpat.errors import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
-from django.contrib import messages 
+from django.urls import reverse 
 
 productos = []
 
@@ -17,43 +18,48 @@ def agregar_producto(request):
         producto_nuevo = Producto(nombre=nombre, precio=precio, cantidad=cantidad)
         producto_nuevo.save()
 
+        # Agregamos el mensaje de éxito
         messages.success(request, 'Producto agregado exitosamente.')
 
         return redirect('listar_productos')
 
     return render(request, 'agregar.html')
 
-def actualizar_producto(request):
-    if request.method == "POST":
-        nombre = request.POST.get("nombre")
-        precio = request.POST.get("precio")
-        cantidad = request.POST.get("cantidad")
-        for producto in productos:
-            if producto.nombre == nombre:
-                producto.precio = precio
-                producto.cantidad = cantidad
-                break
-        return redirect("productos:listar_productos")
+def actualizar_producto(request, id):
+    producto = get_object_or_404(Producto, pk=id)
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+        
+        # Actualiza los campos del producto
+        producto.nombre = nombre
+        producto.precio = precio
+        producto.cantidad = cantidad
+        producto.save()
+        
+        return redirect('productos:listar_productos')
     else:
-        producto_id = request.GET.get('id')
-        producto = Producto.objects.get(pk=producto_id)
-        return render(request, "actualizar.html", {'producto': producto})
+        return render(request, 'actualizar.html', {'producto': producto})
 
 def eliminar_producto(request):
     if request.method == "POST":
         nombre = request.POST.get("nombre")
-        for producto in productos:
-            if producto.nombre == nombre:
-                productos.remove(producto)
-                break
-        return redirect("listar_productos")
+        try:
+            producto = Producto.objects.get(nombre=nombre)
+            producto.delete()
+        except Producto.DoesNotExist:
+            pass
+        
+        return redirect('productos:listar_productos')
     return render(request, "eliminar.html")
 
 def buscar_producto(request):
     if request.method == "POST":
         nombre = request.POST.get("nombre")
-        for producto in productos:
-            if producto.nombre == nombre:
-                return render(request, "buscar.html", {"producto": producto})
-        return render(request, "buscar.html", {"producto": None})
+        try:
+            producto = Producto.objects.get(nombre=nombre)
+            return render(request, "buscar.html", {"producto": producto})
+        except Producto.DoesNotExist:
+            return render(request, "buscar.html", {"producto": None})
     return render(request, "buscar.html")
